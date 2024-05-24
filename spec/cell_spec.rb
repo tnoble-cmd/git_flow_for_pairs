158e4121a88bfa5e19b7a437067e27ca3fd6d755
Sid Swaminathan <sidswaminathan@yahoo.com>
2024-05-24 11:00:28 -0500

    add cell testing spec

---
+++ spec/cell_spec.rb
@@ -0,0 +1,17 @@
+require 'spec_helper'
+
+RSpec.describe Cell do
+  before(:each) do
+    # @cruiser = Ship.new("Cruiser", 3)
+  end
+
+  describe '#initialize' do
+    it 'can initialize' do
+    #   expect(@cruiser).to be_an_instance_of(Ship)
+    #   expect(@cruiser.name).to eq('Cruiser')
+    #   expect(@cruiser.length).to eq(3)
+    #   expect(@cruiser.health).to eq(3)
+    #   expect(@cruiser.sunk?).to eq(false)
+    end
+  end
+end
